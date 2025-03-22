eb786fcb6759a3a0a56912641d494c718e6d2e0f
titauhidd <tauhidultonmoy1234@gmail.com>
2025-03-22 16:07:53 +0600

    Implement redirection to home page

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 document.getElementById("btn-login").addEventListener("click", function (e) {
   e.preventDefault();
   console.log("clicked");
-
+  // get phone & Pin nmbr
   const phnNumber = document.getElementById("phn-number").value;
-  console.log(phnNumber);
+  const pinNumber = document.getElementById("pin-nmbr").value;
+  console.log(phnNumber, pinNumber);
+  if (phnNumber === "5" && pinNumber === "5") {
+    // console.log("You are logged in");
+    window.location.href = "/home.html";
+  } else {
+    alert("Wropng Pin or Phone Number");
+  }
 });
